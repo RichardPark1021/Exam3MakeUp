c9cbe8846467ccb165acb74a526b6af27ab8eda0
RichardPark1021 <rpark1@ggc.edu>
2023-10-25 08:37:13 -0400

    Initial Commit

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class Book {
+public class Book {
+    private String title;
+    private String author;
+    private int yearPublished;
+
+    public Book(String title, String author, int yearPublished) {
+        this.title = title;
+        this.author = author;
+        this.yearPublished = yearPublished;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getYearPublished() {
+        return yearPublished;
+    }
+
+    @Override
+    public String toString() {
+        return "Title: " + title + ", Author: " + author + ", Year Published: " + yearPublished;
+    }
 }
--- src/BookList.java
+++ src/BookList.java
@@ -1,2 +1,47 @@
-package PACKAGE_NAME;public class BookList {
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class BookList {
+    public static void main(String[] args) {
+        BookList booklist = new BookList();
+
+        List<Book> books = new ArrayList<>();
+
+        try(BufferedReader reader = new BufferedReader(new FileReader("src/Nobel.csv"))) {
+            String line;
+            reader.readLine();
+
+            while((line = reader.readLine()) != null) {
+                String[] parts = line.split(",");
+                if(parts.length == 3) {
+                    String title = parts[0];
+                    String author = parts[1];
+                    int yearPublished = Integer.parseInt(parts[2]);
+                    Book book = new Book(title, author, yearPublished);
+                    books.add(book);
+                }
+            }
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+
+        //original Order
+        for(Book book : books) {
+            System.out.println(book);
+        }
+
+        Collections.sort(books, Comparator.comparingInt(Book::getYearPublished));
+
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter("src/output.txt"))) {
+            for (Book book : books) {
+                writer.write(book.getTitle() + "," + book.getAuthor() + "," + book.getYearPublished());
+                writer.newLine();
+            }
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
